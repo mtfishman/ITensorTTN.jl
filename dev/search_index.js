c52245295a90fdee3a480a817bd9192ca0f9d801
Documenter.jl <documenter@juliadocs.github.io>
2021-10-04 13:21:12 +0000

    build based on e1baa1a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ITensorTTN","category":"page"},{"location":"#ITensorTTN","page":"Home","title":"ITensorTTN","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ITensorTTN.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ITensorTTN]","category":"page"}]
+}
